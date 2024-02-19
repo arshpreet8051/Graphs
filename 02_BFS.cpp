651f56a0e86ec9b4a1eaa9b20585c2540b7287db
Arshpreet_Singh <arshpreet8051@gmail.com>
2024-02-19 22:59:19 +0530

    BFS

---
+++ 02_BFS.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+//#include<bits/stdc++.h>
+using namespace std;
+#define or |
+#define and &
+ 
+// Requirements :
+/*                  
+                    -> Ajdacency matrix
+                    -> Queue
+                    -> Visited array
+*/
+
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        
+        vector<int> ans;
+        vector<int> visited(V,0);
+        queue<int> q;
+        
+        q.push(0);
+        visited[0] = 1;
+        
+        
+        while(!q.empty()){
+            
+            int node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            
+            for(auto x : adj[node]){
+                
+                if(visited[x]==0){
+                    visited[x]=1;
+                    q.push(x);
+                }
+            }
+        }
+        
+        return ans;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Graphs
 0. [Adjacent Matrix](00_Adjacent_Matrix.cpp)
 1. [Adjacent List](00_Adjacent_List.cpp)
+1. [BFS](02_BFS.cpp)

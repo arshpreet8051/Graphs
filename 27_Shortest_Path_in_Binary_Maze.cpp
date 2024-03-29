d4054167a4baf5ead5078c088336af8ae8588e54
Arshpreet_Singh <arshpreet8051@gmail.com>
2024-03-29 06:09:03 +0530

    Shortest Path in Binary maze

--- 27_Shortest_Path_in_Binary_Maze.cpp
+++ 27_Shortest_Path_in_Binary_Maze.cpp
@@ -63,3 +63,64 @@ int shortestPathBinaryMatrix(vector<vector<int>> &matrix, pair<int, int> src, pa
     //if(ans==INT_MAX)return -1;
     return -1;
 }
+
+//---------------------------------------------------------------------------------------------------
+
+class Solution {
+  public:
+    int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
+                     pair<int, int> destination) {
+        
+        
+        // we will apply Dijkstra to get the minimum path
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int src_x = source.first;
+        int src_y = source.second;
+        
+        int final_x = destination.first;
+        int final_y = destination.second;
+        
+        // Handling edge cases
+        if((grid[src_x][src_y]!=1) or (grid[final_x][final_y]!=1)){return -1;}
+        
+        // making 2-D array to compare distance every time and only proceed if there is chance of shorter distance
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        dist[src_x][src_y] = 0;
+        
+        int delta_x[4] = {-1,1,0,0};
+        int delta_y[4] = {0,0,-1,1};
+        
+        // Note : Not using priority queue as weight is only unit for every node so simple queue will work
+        queue<vector<int>>q;
+        
+        q.push({src_x,src_y,0});
+        
+        while(!q.empty()){
+            
+            vector<int> front = q.front();
+            int x = front[0];
+            int y = front[1];
+            int len = front[2];
+            
+            q.pop();
+            
+            if(x==final_x and y==final_y){return len;}
+            
+            for(int i = 0;i<4;i++){
+                
+                int new_x = x + delta_x[i];
+                int new_y = y + delta_y[i];
+                
+                if( (new_x>=0 and new_x<n) and (new_y>=0 and new_y<m) and grid[new_x][new_y]==1 and (1 + len < dist[new_x][new_y])){
+                    dist[new_x][new_y] = 1 + len;
+                    q.push({new_x,new_y,len+1});
+                }
+            }
+        }
+        
+        return -1;
+        
+    }
+};
